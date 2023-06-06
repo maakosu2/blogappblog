65560b16b37bf139757af1639e2c63178a8e4c05
Akosu_MandelE <mandelaakossu@gmail.com>
2023-06-06 16:53:02 +0100

    post component completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import {FaYoutube, FaTwitter, FaGithub, FaLaptop} from "react-icons/fa"
+import {FaLinkedin, FaTwitter, FaGithub, FaLaptop} from "react-icons/fa"
 export default function Navbar() {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
@@ -9,16 +9,16 @@ export default function Navbar() {
                     <Link href="/" className="text-white/90 no-underline hover:text-white">Akosu Aondohemba Mandela</Link>
                 </h1>
                 <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
-                    <Link className="text-white/90 hover:text-white" href="https://www.youtube.com/@DaveGrayTeachesCode">
-                        <FaYoutube />
+                    <Link className="text-white/90 hover:text-white" href="https://www.linkedin.com/in/mandela-aondohemba-akosu-23055aab/">
+                        <FaLinkedin />
                     </Link>
-                    <Link className="text-white/90 hover:text-white" href="https://courses.davegray.codes/">
+                    <Link className="text-white/90 hover:text-white" href="#">
                         <FaLaptop />
                     </Link>
-                    <Link className="text-white/90 hover:text-white" href="https://github.com/gitdagray">
+                    <Link className="text-white/90 hover:text-white" href="https://github.com/maakosu2/">
                         <FaGithub />
                     </Link>
-                    <Link className="text-white/90 hover:text-white" href="https://twitter.com/yesdavidgray">
+                    <Link className="text-white/90 hover:text-white" href="#">
                         <FaTwitter />
                     </Link>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Posts from './components/Posts'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
           I&apos;m <span className="font-bold">Akosu Aondohemba Mandela</span>.
         </span>
       </p>
+      <Posts/>
     </main>
   )
 }
